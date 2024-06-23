427cf399874ad78031b95aa491b64fecf2835f7f
hrajadurai <118383577+hrajadurai@users.noreply.github.com>
2024-06-22 20:11:14 -0400

    Add files via upload

---
+++ ship_helper-2.py
@@ -0,0 +1,29 @@
+import pandas as pd, numpy as np
+
+def extractTitle(name):
+    if "." in name:
+        return(name.split(".")[0].split(",")[1].strip())
+    else:
+        return("No title")
+
+def cleanShipData(data):
+    df=data.copy()
+    df['Title'] = df['Name'].apply(extractTitle)
+
+    # fill the missing age value
+    df["Age"] = df.groupby('Title')['Age'].transform(lambda x:x.fillna(x.median()))
+
+    # dropping Cabin as it has too many missing values
+    df.drop(columns='Cabin',inplace=True)
+
+    # deleting rows where embarked is missing
+    df.dropna(subset=['Embarked'],inplace=True)
+
+    # create TravelAlone
+    df['Travelalone'] =  np.where((df['SibSp'] + df['Parch']) > 0, 0 , 1).astype('uint8')
+
+    # dropping features which are not relevant
+    df.drop(columns=['PassengerId','Name','SibSp','Parch','Ticket','Title'],inplace=True)
+
+    df_dummy = pd.get_dummies(df,columns=['Sex','Embarked'],drop_first=True,dtype = int)
+    return(df_dummy)
